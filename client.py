a4efde1f40b5666766c9ee8acc4d7a94f4d1a870
abel <abelj@uci.edu>
2019-11-14 18:12:55 -0800

    fixed ip

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import grpc
 import calculator_pb2
 import calculator_pb2_grpc
 
-channel = grpc.insecure_channel('localhost:50051')
+channel = grpc.insecure_channel('127.0.1.1:50051')
 
 stub = calculator_pb2_grpc.CalculatorStub(channel)
 
